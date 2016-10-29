6381fc764e2cf46ff78e7f8e2aa7b22fec94f469
Alex Gru <grusch.alex@gmail.com>
2016-10-29 23:17:32 +0200

    first step: print file line-by-line using filename from command line arg; added smaller textfile (just temp)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # similarity_search
+
+### win command (git bash, cmake not working yet) 
+clear && rm -f main.exe && g++.exe main.cxx -o main.exe && ./main.exe ../dblp_first500.txt
--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main (int argc, char *argv[]) {
+
+	if ( argc != 2 ) {
+		std::cout<<"usage: "<< argv[0] <<" <filename>\n";
+	}
+	else {
+	  std::cout << "File contents:" << std::endl << std::endl;
+		std::ifstream infile(argv[1]);
+
+		std::string line;
+		while (std::getline(infile, line))
+		{
+	    std::istringstream iss(line);
+	    std::cout << line << std::endl;
+		}
+		return 0;
+	}
+	return -1;
 }

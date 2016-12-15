22e103ad7a0a075502fb06ad633b568781c6a7f1
Alex Gru <grusch.alex@gmail.com>
2016-12-15 22:16:54 +0100

    - Jaccard gets parameters: posr1 posr2 foundoverlap (as discussed last time)

--- src/jaccard.h
+++ src/jaccard.h
@@ -4,11 +4,9 @@
 #include <vector>
 
 // are sets similar with respect to a given threshold?
-inline bool jaccard(const std::vector<int> r1, const std::vector<int> r2, double threshold) {
+inline bool jaccard(const std::vector<int> r1, const std::vector<int> r2, double threshold, unsigned int posr1 = 0,
+                    unsigned int posr2 = 0, unsigned int foundoverlap = 0) {
     // taken from original implementation (verify.h), including optimizations
-    unsigned int posr1 = 0;
-    unsigned int posr2 = 0;
-    unsigned int foundoverlap = 0;
     unsigned int overlapthres = (unsigned int) (ceil((r1.size() + r2.size()) * threshold / (1 + threshold)));
 
     unsigned int maxr1 = r1.size() - posr1 + foundoverlap;

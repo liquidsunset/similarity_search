39440729520b1f04f45d252680165b052e1ea884
Daniel Brand <branddan@stud.sbg.ac.at>
2017-01-10 15:58:40 +0100

    fix allpairs
    TODO: check if calc. is done right :)

--- src/allpairs.h
+++ src/allpairs.h
@@ -9,6 +9,7 @@ typedef google::dense_hash_map<int, std::vector<int>> inverted_list;
 typedef inverted_list::iterator list_iterator;
 
 inline static unsigned int maxprefix(unsigned int len, double threshold);
+inline static unsigned int minoverlap(unsigned int len1, unsigned int len2, double threshold);
 
 struct record {
     unsigned int candidate_count = 0;
@@ -22,16 +23,21 @@ void allPairs(std::vector<int> &set_vector, int set_idx, double jaccard_threshol
 
     unsigned int maxpref = maxprefix(set_vector.size(), jaccard_threshold);
 
-    for (auto i = set_vector.begin(); i != set_vector.end(); ++i) {
+    //for (auto i = set_vector.begin(); i != set_vector.end(); ++i) {
+    for (unsigned recpos = 0; recpos < maxpref; ++recpos) {
 
-        list_iterator token_id = inv_list.find(*i);
+        int i = set_vector[recpos];
+        list_iterator token_id = inv_list.find(i);
 
-        //TODO: maxprefix berechnen
         if (token_id != inv_list.end()) {
 //        std::cout << "found in I: " << token_id.first << std::endl;
             // add set indexes, where token occurs in, to candidate set
             for (auto set = token_id->second.begin(); set != token_id->second.end(); ++set) {
                 record &curr_set = all_sets.at(*set);
+                unsigned int indreclen = curr_set.tokens.size();
+                if(indreclen < maxpref) {
+                    break;
+                }
                 if (curr_set.candidate_count == 0) // first check if 0, increment afterwards
                     candidate_indexes.push_back(*set);
                 curr_set.candidate_count++;
@@ -74,7 +80,9 @@ void allPairs(std::vector<int> &set_vector, int set_idx, double jaccard_threshol
             indrecpos = candidate.candidate_count;
         }
 
-        bool similar = jaccard(set_vector, candidate.tokens, jaccard_threshold, recpos,
+        unsigned int min = minoverlap(set_vector.size(), candidate.tokens.size(), jaccard_threshold);
+
+        bool similar = jaccard(set_vector, candidate.tokens, min, recpos,
                                indrecpos, candidate.candidate_count);
 
         if (similar) {
@@ -92,6 +100,10 @@ inline static unsigned int minsize(unsigned int len, double threshold) {
 
 inline static unsigned int maxprefix(unsigned int len, double threshold) {
     return std::min(len, len - minsize(len, threshold) + 1);
+}
+
+inline static unsigned int minoverlap(unsigned int len1, unsigned int len2, double threshold) {
+    return std::min(len2, std::min(len1, (unsigned int) (ceil((len1 + len2) * threshold / (1 + threshold)))));
 }
 
 #endif //ALLPAIRS_H
